9168df4589adb2e3cb6ef797d42e442649b96d5a
Joao <silva.joao1@outlook.com.br>
2021-04-25 22:49:51 -0300

    Change api url to production instead development

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.10:3333',
-  // baseURL: 'https://api.shishapedia.com.br',
+  baseURL: 'https://api.shishapedia.com.br',
 });
 
 export default api;
